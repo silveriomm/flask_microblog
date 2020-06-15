1f27941a09daa9d51e986f62e880b0a20cbc3e91
Silvério Mantovaneli <silveriomm@bol.com.br>
2020-06-15 20:53:34 -0300

    Login finalizado com redicionamento da page home para login

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,17 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user, logout_user
-from app.models import User
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User, Post
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-  user = {'username': 'Miguel'}
-  return render_template('index.html', title='Home', user=user)
+  posts = Post.query.all()
+  return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -21,9 +23,11 @@ def login():
       flash('Invalid username or password')
       return redirect(url_for('login'))
     login_user(user, remember=form.remember_me.data)
-    return redirect(url_for('index'))
-    return redirect('/index')
-  return render_template('login.html', title='Sign In', form=form)
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+      next_page = url_for('index')
+    return redirect(next_page)
+  return render_template('login.html', title='Login', form=form)
 
 
 @app.route('/logout')
