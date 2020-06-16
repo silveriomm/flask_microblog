7ede4827911c9fa1303ba0abf3c5ef9dcec9f91f
Silvério Mantovaneli <silveriomm@bol.com.br>
2020-06-16 20:33:04 -0300

    Profile page and avatars

--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ from werkzeug.security import (
 
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
+
 
 @login.user_loader
 def load_user(id):
@@ -18,6 +20,8 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
   posts = db.relationship('Post', backref='author', lazy='dynamic')
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
@@ -27,6 +31,10 @@ class User(UserMixin, db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 class Post(db.Model):
